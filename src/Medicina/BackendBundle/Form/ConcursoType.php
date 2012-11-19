075c1d0493eac9f666ca8866a78073b8af7db64a
juan urtazun <juan081@gmail.com>
2012-11-19 18:38:24 -0300

    Concurso agregado crud basico Agregado  jqueryui 1.9 jquery 1.8.2 Agregado esto al twig base

--- src/Medicina/BackendBundle/Form/ConcursoType.php
+++ src/Medicina/BackendBundle/Form/ConcursoType.php
@@ -10,11 +10,36 @@ class ConcursoType extends AbstractType
     public function buildForm(FormBuilder $builder, array $options)
     {
         $builder
-            ->add('fechaInicioExpediente')
-            ->add('fechaCierre')
-            ->add('fechaInicioInscripcion')
-            ->add('fechaEvaluacion')
-            ->add('fechaEntregaActa')
+            ->add('fechaInicioExpediente','date',array(
+            'widget' => 'single_text',
+            'format' => 'dd/MM/yyyy',
+            'attr' => array('class' => 'date'),
+            'label'=>"Fecha de Inicio"    
+        ))
+            ->add('fechaCierre','date',array(
+            'widget' => 'single_text',
+            'format' => 'dd/MM/yyyy',
+            'attr' => array('class' => 'date'),
+            'label'=>"Fecha de Cierre"    
+        ))
+            ->add('fechaInicioInscripcion','date',array(
+            'widget' => 'single_text',
+            'format' => 'dd/MM/yyyy',
+            'attr' => array('class' => 'date'),
+            'label'=>"Fecha de Inicio Inscripción"    
+        ))
+            ->add('fechaEvaluacion','date',array(
+            'widget' => 'single_text',
+            'format' => 'dd/MM/yyyy',
+            'attr' => array('class' => 'date'),
+            'label'=>"Fecha de Evaluación"    
+        ))
+            ->add('fechaEntregaActa','date',array(
+            'widget' => 'single_text',
+            'format' => 'dd/MM/yyyy',
+            'attr' => array('class' => 'date'),
+            'label'=>"Fecha de Entrega del Acta"    
+        ))
             ->add('numeroExpediente')
             ->add('cerrado')
             ->add('oficina')
